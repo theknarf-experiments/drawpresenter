aa69bb0d4930f659f05da9bb27b571907184dde2
Frank Lyder Bredland <fl@nyggen.com>
2022-11-23 21:41:52 +0100

    Update useSlides to use a clamp function

--- src/useSlides.js
+++ src/useSlides.js
@@ -2,14 +2,17 @@ import { useReducer } from 'react';
 
 const useSlides = (doc) => {
 	const max = doc.sections.length - 1;
+	const clamp = (value, min, max) => {
+		return Math.min(Math.max(value, min), max);
+	};
 
 	const [state, dispatch] = useReducer((state, action) => {
 		if(action.type == 'next') {
-			return (state + 1) <= max ? (state + 1) : max;
+			return clamp(state + 1, 0, max);
 		} else if(action.type == 'prev') {
-			return (state - 1) >= 0 ? (state - 1) : 0;
+			return clamp(state - 1, 0, max);
 		} else if(action.type == 'goto') {
-			return action.value;
+			return clamp(action.value, 0, max);
 		}
 
 		return state;
