0d5f0b43e49f18887804a1270486ec6b52cabe3b
Frank Lyder Bredland <fl@nyggen.com>
2022-11-24 16:45:26 +0100

    Fullscreen button with `f`

--- src/pages/present.jsx
+++ src/pages/present.jsx
@@ -14,6 +14,17 @@ export async function getServerSideProps(context) {
   }
 }
 
+const openFullscreen = () => {
+	const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
 // Shows a bar at the bottom of the presentation giving a hint on how far in the presentation you are
 const StatusIndicator = ({ doc, currentSlide }) => {
 	const max = doc.sections.length - 1;
@@ -31,6 +42,7 @@ const Present = ({ doc }) => {
 		'ArrowRight': next,
 		'ArrowUp': prev,
 		'ArrowLeft': prev,
+		'f': openFullscreen,
 	});
 
 	return <div className={`${themeA} ${present}`}>
